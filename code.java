2ff6b3488f71978f1b1369688d6c9abaebb0f824
HarshithaRavindar <60246794+HarshithaRavindar@users.noreply.github.com>
2020-02-07 13:16:26 +0530

    Update code.java

--- code.java
+++ code.java
@@ -4,8 +4,8 @@ class Add
 public static void main(String[] arg)
 {
 int a,b,c
-Scanner sc=new Scanner(System.in)
-System.out.println("Enter first number")
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter first number");
 a=sc.nextInt();
 System.out.println("Enter second number")
 b=sc.nextInt()
